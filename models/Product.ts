2886167efe3db68c8b4f8f58335d5c0a0c97cf01
Mikhail_Mir <mirzaakhmetov@infodev.ru>
2020-05-30 16:03:20 +1000

    Выполнено задание к пеосрму уроку

---
+++ models/Product.ts
@@ -0,0 +1,18 @@
+export class Product {
+    title: string;
+    shortDescription: string;
+    fullDescription: string;
+    features: string[];
+    imageURL: string;
+
+    constructor(
+        title: string, shortDescription: string, fullDescription: string,
+        features: string[], imageURL: string
+    ) {
+        this.title = title
+        this.shortDescription = shortDescription
+        this.fullDescription = fullDescription
+        this.features = features
+        this.imageURL = imageURL
+    }
+}
