8fbcdf66d0f9a7b0faf935d079159290f254ae59
Mikhail_Mir <mirzaakhmetov@infodev.ru>
2020-06-03 17:52:45 +1000

    Выполннены задания к 5 уроку

--- asserts/js/header.js
+++ asserts/js/header.js
@@ -1,9 +1,9 @@
 new Vue ({
   el: '#header-block',
-  template: '<div class="row"><img src="asserts/img/magic-trick.svg" id="logo" alt="Логотип"><h1>{{ message }}</h1></div>',
+  template: '<div class="row"><img src="asserts/img/magic-trick.svg" id="logo" alt="Логотип"><h1 id="logo-msg">{{ message }}</h1></div>',
   data () {
     return {
-      message: 'Be a sure and dont be sucks, find your magic in the box!'
+      message: 'Find your magic!'
     }
   }
 })
