c33787690b0f319fb273a16a90633eb83c696f66
Mikhail_Mir <mirzaakhmetov@infodev.ru>
2020-06-05 11:58:55 +1000

    all done

--- models/Product.ts
+++ models/Product.ts
@@ -6,13 +6,19 @@ export class Product {
     imageURL: string;
 
     constructor(
-        title: string, shortDescription: string, fullDescription: string,
-        features: string[], imageURL: string
+        title: string,
+        shortDescription: string,
+        fullDescription: string,
+        features: string[],
+        imageURL: string
     ) {
         this.title = title
         this.shortDescription = shortDescription
         this.fullDescription = fullDescription
         this.features = features
         this.imageURL = imageURL
+    }
+    sayHi (): string {
+        return 'hi'
     }
 }
