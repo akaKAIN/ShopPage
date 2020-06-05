80ab3f4f09feec2d10222df76b9d5d434ed04d27
Mikhail_Mir <mirzaakhmetov@infodev.ru>
2020-06-05 15:37:59 +1000

    init new site-page

--- asserts/js/header.js
+++
@@ -1,9 +0,0 @@
-new Vue ({
-  el: '#header-block',
-  template: '<div class="row"><img src="asserts/img/magic-trick.svg" id="logo" alt="Логотип"><h1 id="logo-msg">{{ message }}</h1></div>',
-  data () {
-    return {
-      message: 'Find your magic!'
-    }
-  }
-})
