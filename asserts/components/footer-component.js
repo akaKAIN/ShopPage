9e9302fb33749b69ee57de50abf26f990a67bb6d
Mikhail_Mir <mirzaakhmetov@infodev.ru>
2020-06-08 15:23:01 +1000

    Выполнено ДЗ №6

---
+++ asserts/components/footer-component.js
@@ -0,0 +1,58 @@
+new Vue({
+  el: '#app-footer',
+  template: '<div class="footer-wrapper row d-flex justify-content-between p-rel">\n' +
+    '    <div class="left-container">\n' +
+    '      <div class="footer-title">Contact Us</div>\n' +
+    '      <div class="col-text">\n' +
+    '        132A Bridge Road Richmond VIC Australia.\n' +
+    '      </div>\n' +
+    '      <div class="col-text">Talk to us on 1300 132 info@interior.com\n' +
+    '      </div>\n' +
+    '    </div>\n' +
+    '    <div class="middle-container">\n' +
+    '      <div class="footer-title">Useful Information</div>\n' +
+    '      <div class="col-text-wrapper row ml-1 mr-1">\n' +
+    '        <div class="text-left">\n' +
+    '          <ul class="footer-ul">\n' +
+    '            <li>Sales terms</li>\n' +
+    '            <li>Customer care</li>\n' +
+    '            <li>Delivery</li>\n' +
+    '          </ul>\n' +
+    '        </div>\n' +
+    '        <div class="text-right">\n' +
+    '          <ul class="footer-ul">\n' +
+    '            <li>Architect accounts</li>\n' +
+    '            <li>Careers</li>\n' +
+    '            <li>Exchanges & returns</li>\n' +
+    '          </ul>\n' +
+    '        </div>\n' +
+    '      </div>\n' +
+    '    </div>\n' +
+    '    <div class="right-container">\n' +
+    '      <div class="footer-title">\n' +
+    '        Let’s Stay in Touch!\n' +
+    '      </div>\n' +
+    '      <div class="col-text">\n' +
+    '        Suscribe to know about our latest news, products and special offers just for suscribers.\n' +
+    '      </div>\n' +
+    '      <form class="send-form p-rel" action="#">\n' +
+    '        <input type="email" class="client-email p-abs" placeholder="your email address">\n' +
+    '        <input class="up-2 p-abs" type="submit" value="">\n' +
+    '      </form>\n' +
+    '\n' +
+    '    </div>\n' +
+    '    <div class="bottom-text">\n' +
+    '      <div class="row d-flex justify-content-between ml-1 mr-1">\n' +
+    '        <div class="bottom-text-container">\n' +
+    '          &copy;&nbsp;Copyright&nbsp;&mdash; INTERIOR 2016. All Rights Reserved.\n' +
+    '        </div>\n' +
+    '        <div class="bottom-text-container">\n' +
+    '          Terms &amp;&nbsp;Conditions&nbsp;/ Privacy policy &amp;&nbsp;Cookies\n' +
+    '        </div>\n' +
+    '      </div>\n' +
+    '    </div>\n' +
+    '    <div class="social-apps-row">\n' +
+    '      <img src="./asserts/img/social-links.jpg" alt="socials links">\n' +
+    '    </div>\n' +
+    '  </div>'
+});
